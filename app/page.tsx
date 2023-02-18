4edbe3e5d8ad9b4f3a4accb0cd90c6c625a8352d
Murat Altun <mraltun@gmail.com>
2023-02-18 13:30:34 +0300

    Added Navbar to Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,24 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Hello there</h1>
+    <main className='bg-gray-100 min-h-screen w-screen'>
+      <main className='max-w-screen-2xl m-auto bg-white'>
+        {/* NAVBAR */}
+        <nav className='bg-white p-2 flex justify-between'>
+          <a href='' className='font-bold text-gray-700 text-2xl'>
+            Restaurant
+          </a>
+          <div>
+            <div className='flex'>
+              <button className='bg-blue-400 text-white border mr-3 p-1 px-4 rounded'>
+                Sign In
+              </button>
+              <button className='border p-1 px-4 rounded'>Sign Up</button>
+            </div>
+          </div>
+        </nav>
+        {/* NAVBAR */}
+      </main>
     </main>
   );
 }
