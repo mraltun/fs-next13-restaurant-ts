e133d1f8a26e7285b15128d0fec31bd1d7b12757
Murat Altun <mraltun@gmail.com>
2023-02-20 14:09:02 +0300

    Seperated RestaurantCard to it's own component

---
+++ app/components/RestaurantCard.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+export default function RestaurantCard() {
+  return (
+    <div className='w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer'>
+      <Link href='/restaurant/adana-kebab'>
+        <img
+          src='https://images.pexels.com/photos/1404819/pexels-photo-1404819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+          alt=''
+          className='w-full h-36'
+        />
+        <div className='p-1'>
+          <h3 className='font-bold text-2xl mb-2'>Cat Food</h3>
+          <div className='flex items-start'>
+            <div className='flex mb-2'>*****</div>
+            <p className='ml-2'>77 reviews</p>
+          </div>
+          <div className='flex text-reg font-light capitalize'>
+            <p className='mr-3'>Mexican</p>
+            <p className='mr-3'>$$$$</p>
+            <p className='mr-3'>Istanbul</p>
+          </div>
+          <p className='text-sm mt-1 font-bold'>Booked 3 times today</p>
+        </div>
+      </Link>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
 import styles from "./page.module.css";
+import Card from "./components/RestaurantCard";
 
 export default function Home() {
   return (
@@ -13,28 +14,7 @@ export default function Home() {
           {/* CARDS */}
           <div className='py-3 px-36 mt-10 flex flex-wrap'>
             {/* CARD */}
-            <div className='w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer'>
-              <Link href='/restaurant/adana-kebab'>
-                <img
-                  src='https://images.pexels.com/photos/1404819/pexels-photo-1404819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-                  alt=''
-                  className='w-full h-36'
-                />
-                <div className='p-1'>
-                  <h3 className='font-bold text-2xl mb-2'>Cat Food</h3>
-                  <div className='flex items-start'>
-                    <div className='flex mb-2'>*****</div>
-                    <p className='ml-2'>77 reviews</p>
-                  </div>
-                  <div className='flex text-reg font-light capitalize'>
-                    <p className='mr-3'>Mexican</p>
-                    <p className='mr-3'>$$$$</p>
-                    <p className='mr-3'>Istanbul</p>
-                  </div>
-                  <p className='text-sm mt-1 font-bold'>Booked 3 times today</p>
-                </div>
-              </Link>
-            </div>
+            <Card />
             {/* CARD */}
           </div>
           {/* CARDS */}
