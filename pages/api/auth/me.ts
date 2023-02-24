c695aff9eb2e2d67a2e2a5d8421a78ca83966c41
Murat Altun <mraltun@gmail.com>
2023-02-24 18:38:35 +0300

    Added middleware and refactor me

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
-import * as jose from "jose";
 import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
@@ -10,26 +9,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const bearerToken = req.headers["authorization"] as string;
-
-  if (!bearerToken) {
-    res.status(401).json({ errorMessage: "Unauthorized request (No bearer)" });
-  }
-
   const token = bearerToken.split(" ")[1];
-
-  if (!token) {
-    res.status(401).json({ errorMessage: "Unauthorized request (No token)" });
-  }
-
-  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-
-  try {
-    await jose.jwtVerify(token, secret);
-  } catch (error) {
-    return res
-      .status(401)
-      .json({ errorMessage: "Unauthorized request (Token invalid)" });
-  }
 
   const payload = jwt.decode(token) as { email: string };
 
