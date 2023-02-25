4aa8d5abdab1f86a67e2283b2f4a576047b6dc36
Murat Altun <mraltun@gmail.com>
2023-02-25 12:27:19 +0300

    Added useAuth hook

--- app/components/AuthModal.tsx
+++ app/components/AuthModal.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import AuthModalInputs from "./AuthModalInputs";
+import useAuth from "@/hooks/useAuth";
 
 const style = {
   position: "absolute" as "absolute",
@@ -20,6 +21,7 @@ export default function AuthModal({ isSignin }: { isSignin: boolean }) {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const { signin, signup } = useAuth();
 
   const renderContent = (signinContent: string, signupContent: string) => {
     return isSignin ? signinContent : signupContent;
@@ -64,6 +66,12 @@ export default function AuthModal({ isSignin }: { isSignin: boolean }) {
     setDisabled(true);
   }, [inputs]);
 
+  const handleClick = () => {
+    if (isSignin) {
+      signin({ email: inputs.email, password: inputs.password });
+    }
+  };
+
   return (
     <div>
       <button
@@ -103,6 +111,7 @@ export default function AuthModal({ isSignin }: { isSignin: boolean }) {
               <button
                 className='uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400'
                 disabled={disabled}
+                onClick={handleClick}
               >
                 {renderContent("Sign In", "Create Account")}
               </button>
