867203584b186a05b726c53ae92d91e737a9bf99
Murat Altun <mraltun@gmail.com>
2023-02-27 18:06:30 +0300

    Fetch the bookings

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -1,5 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
 import { times } from "../../../../data";
+
+const prisma = new PrismaClient();
 
 export default async function handler(
   req: NextApiRequest,
@@ -24,5 +27,19 @@ export default async function handler(
     return res.status(400).json({ errorMessage: "Invalid data provided" });
   }
 
-  return res.json({ searchTimes });
+  const bookings = await prisma.booking.findMany({
+    where: {
+      booking_time: {
+        gte: new Date(`${day}T${searchTimes[0]}`),
+        lte: new Date(`${day}T${searchTimes[searchTimes.length - 1]}`),
+      },
+    },
+    select: {
+      number_of_people: true,
+      booking_time: true,
+      tables: true,
+    },
+  });
+
+  return res.json({ searchTimes, bookings });
 }
