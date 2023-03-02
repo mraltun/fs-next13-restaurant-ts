4268402fc63e90458b45df21eba3f42bc30d380f
Murat Altun <mraltun@gmail.com>
2023-03-02 16:25:57 +0300

    Count the tables and determine tables to book

--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -58,5 +58,45 @@ export default async function handler(
       .json({ errorMessage: "No availability, cannot book" });
   }
 
+  const tablesCount: {
+    2: number[];
+    4: number[];
+  } = { 2: [], 4: [] };
+
+  searchTimeWithTables.tables.forEach((table) => {
+    if (table.seats === 2) {
+      tablesCount[2].push(table.id);
+    } else {
+      tablesCount[4].push(table.id);
+    }
+  });
+
+  const tablesToBooks: number[] = [];
+  let seatsRemaining = parseInt(partySize);
+
+  while (seatsRemaining > 0) {
+    if (seatsRemaining >= 3) {
+      if (tablesCount[4].length) {
+        tablesToBooks.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining = seatsRemaining - 4;
+      } else {
+        tablesToBooks.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining = seatsRemaining - 2;
+      }
+    } else {
+      if (tablesCount[2].length) {
+        tablesToBooks.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining = seatsRemaining - 2;
+      } else {
+        tablesToBooks.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining = seatsRemaining - 4;
+      }
+    }
+  }
+
   return res.json({ searchTimeWithTables });
 }
